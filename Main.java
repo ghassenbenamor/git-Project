7b77375336b6ae37d3a12bb138097b871db3c4b8
Ghassen Ben Amor <ghassen.benamor17@gmail.com>
2022-11-17 12:43:20 +0100

    increment hiba

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String[] args) {
-        
+        public static void increment(int n) {
+            int n1 = n+20; //hiba wants to increment by 20
+            System.out.println(n1);
+            
+        } 
     }
 }
