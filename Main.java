518928e33a5522c91c6bb6165d925c2e854b5f47
Ghassen Ben Amor <ghassen.benamor17@gmail.com>
2022-11-17 12:39:26 +0100

    increment

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        
+        public static void increment(int n) {
+            int n1 = n+1;
+            System.out.println(n1);
+            
+        }
     }
 }
